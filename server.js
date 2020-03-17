dbc203554f0407095c047bbd08927247ab6e5571
CoreyDeJong <dejongcorey@gmail.com>
2020-03-17 10:32:10 -0700

    initial files created, ready for deployment

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+app.use(cors());
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
